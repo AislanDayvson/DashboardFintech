3709673122b2022e63b45ee167f065be6d01fc83
Aislan <aislandnjunior@gmail.com>
2024-10-30 19:56:00 -0300

    ✅ feat: adicione itens para a sidenav
    -adição de itens para a pasta Assets
    -criação da sidenav
    -estilização da sidenav
    -criação de titulo dependendo da página

--- src/Components/Sidenav.tsx
+++ src/Components/Sidenav.tsx
@@ -1,8 +1,21 @@
-import React from 'react'
-
+import resumo from '../assets/icons/resumo.svg'
+import vendas from '../assets/icons/vendas.svg'
+import FintechSVG from '../assets/FintechSVG'
 const Sidenav = () => {
   return (
-    <div>Sidenav</div>
+    <nav className='sidenav box bg-3'>
+      <FintechSVG title='fintech logo' />
+      <ul>
+        <li>
+          <span><img src={resumo} alt="" /></span>
+          <a href="">Resumo</a>
+        </li>
+        <li>
+          <span><img src={vendas} alt="" /></span>
+          <a href="">Vendas</a>
+        </li>
+      </ul>
+    </nav>
   )
 }
 
