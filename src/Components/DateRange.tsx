3ec76f6d4775f14212310a0defc84edd5aa36944
Aislan <aislandnjunior@gmail.com>
2024-10-30 17:51:14 -0300

    💄 style: adicione arquivo css base
    - crie variáveis para utilizar durante o arquivo css
    - resete estilos padrões
    - crie classes utilitárias

--- src/Components/DateRange.tsx
+++ src/Components/DateRange.tsx
@@ -5,7 +5,7 @@ const DateRange = () => {
     const [inicio, setInicio] = React.useState('')
     const [final, setFinal] = React.useState('')
   return (
-    <form onSubmit={(e) => e.preventDefault()}>
+    <form className='box flex' onSubmit={(e) => e.preventDefault()}>
         <DateInput value={inicio} label='Início' onChange={({target}) => setInicio(target.value)} />
             {inicio}
         <DateInput value={final} label='Final' onChange={({target}) => setFinal(target.value)} />
